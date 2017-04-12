531bcda7477cf6aa69f5e167d043650be1b9fe1e
zhangquan <zhangquan@mapbar.com>
2017-04-12 11:10:07 +0800

    add  activity modle 中的横竖屏切换

--- activity/src/main/java/com/review/activity/ConfigChangeActivity.java
+++ activity/src/main/java/com/review/activity/ConfigChangeActivity.java
@@ -1,9 +1,14 @@
 package com.review.activity;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,11 +18,13 @@ import android.widget.Toast;
  * 1、显示不同的布局
  * 取名相同的layout，横屏的布局放在layout-land下
  * 2、进入页面时(调用onCreate)加载布局 ，横竖屏layout中相同的id的控件能够使用，不同的id只能在onCreate加载的layout中使用。
+ * 3、对于Fragment，横竖屏切换与Activity类似，会调用onConfigurationChanged，不会走其他生命周期函数。
  */
 
-public class ConfigChangeActivity extends Activity {
+public class ConfigChangeActivity extends FragmentActivity {
     private TextView textView1;
-    private Button btn1;
+    private Button btn_portarit; //只有竖屏才有
+    private Button btn_land; //只有横屏才有
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +32,30 @@ public class ConfigChangeActivity extends Activity {
         setContentView(R.layout.configchange);
         System.out.println("------onCreate");
         textView1 = (TextView) findViewById(R.id.tv1);
-        btn1 = (Button) findViewById(R.id.btn1);
-        Button btn2 = (Button) findViewById(R.id.btn2);
-        System.out.println("btn2=" + btn2);
+        btn_portarit = (Button) findViewById(R.id.btn_portarit);
+        btn_land = (Button) findViewById(R.id.btn_land);
+
+        if (null != btn_portarit) {
+            btn_portarit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ConfigChangeActivity.this, "点击竖屏按钮", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if (null != btn_land) {
+            btn_land.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ConfigChangeActivity.this, "点击横屏按钮", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.contentLayout, MyFragment.instantiate(this, MyFragment.class.getName())).commit();
+
     }
+
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -37,15 +64,99 @@ public class ConfigChangeActivity extends Activity {
         if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
             System.out.println("-----竖屏,textView=" + textView1);
             textView1.setText("切换到竖屏");
+
         } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             System.out.println("-----横屏,textView=" + textView1);
             textView1.setText("切换到横屏");
-            findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(ConfigChangeActivity.this, "点击", Toast.LENGTH_SHORT).show();
-                }
-            });
         }
+    }
+
+    public static class MyFragment extends Fragment {
+        private TextView textView;
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            System.out.println("MyFragment-------onAttach");
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            System.out.println("MyFragment-------onCreate");
+        }
+
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            System.out.println("MyFragment-------onCreateView");
+            textView = new TextView(getContext());
+            textView.setText(isPortarit(getContext())?"竖屏(Frament)":"橫屏(Frament)");
+            textView.setTextSize(20);
+            return textView;
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            System.out.println("MyFragment-------onActivityCreated");
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            System.out.println("MyFragment-------onStart");
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            System.out.println("MyFragment-------onResume");
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            System.out.println("MyFragment-------onPause");
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            System.out.println("MyFragment-------onDestroyView");
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            System.out.println("MyFragment-------onDestroy");
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            System.out.println("MyFragment-------onDetach");
+        }
+
+        @Override
+        public void onConfigurationChanged(Configuration newConfig) {
+            super.onConfigurationChanged(newConfig);
+            System.out.println("Fragment------------onConfigurationChanged,,orientation=" + newConfig.orientation);
+            if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                textView.setText("竖屏(Frament)");
+
+            } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                textView.setText("横屏(Fragment)");
+            }
+        }
+    }
+
+
+    public static boolean isPortarit(Context ctx){
+        if (ctx.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            return true;
+        }
+       return false;
     }
 }
